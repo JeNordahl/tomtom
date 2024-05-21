9bf3d2738d3ac6f4ab5c0c7283901b2690ebd21d
JeNordahl <jesper.nordahl@hotmail.com>
2024-05-21 15:08:45 +0200

    finally working as intended, before adding in the searchfield

--- src/app/components/Map/index.jsx
+++ src/app/components/Map/index.jsx
@@ -1,17 +1,58 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import tt from '@tomtom-international/web-sdk-maps';
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 import ttServices from '@tomtom-international/web-sdk-services';
 
 const MapTest = () => {
+    const mapElement = useRef();
     const map = useRef();
-    const [query, setQuery] = useState("");
-    const [results, setResults] = useState([]);
+
+    function createRoute() {
+        const routeOptions = {
+            key: 'AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU',
+            locations: [
+                [4.91191, 52.36619],
+                [4.87631, 52.36366],
+            ],
+            vehicleCommercial: true,
+            vehicleHeading: 0,
+        };
+
+        ttServices.services.calculateRoute(routeOptions).then((response) => {
+            routeOptions.locations.forEach((location) => 
+                new tt.Marker().setLngLat(location).addTo(map.current)
+            );
+
+            const geojson = response.toGeoJson();
+            map.current.addLayer({
+                id: "route" + Math.random().toString(36).substr(2, 9),
+                type: "line",
+                source: {
+                    type: "geojson",
+                    data: geojson,
+                },
+                paint: {
+                    "line-color": "#0f8ae2",
+                    "line-width": 8,
+                },
+            });
+
+            const bounds = new tt.LngLatBounds();
+            geojson.features[0].geometry.coordinates.forEach((point) => {
+                bounds.extend(tt.LngLat.convert(point)); // creates a bounding area
+            });
+            map.current.fitBounds(bounds, {
+                duration: 300,
+                padding: 50,
+                maxZoom: 14,
+            }); // zooms the map to the searched route
+        });
+    }
 
     useEffect(() => {
         map.current = tt.map({
             key: "AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU",
-            container: 'map',
+            container: mapElement.current,
             zoom: 2,
         });
 
@@ -24,7 +65,45 @@ const MapTest = () => {
 
     }, []);
 
-    useEffect(() => {
+    return (
+        <div>
+            <div className="container">
+                <div ref={mapElement} style={{ height: '500px', width: '100%' }} />
+                <button
+                    className="btn"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        createRoute();
+                    }}
+                >
+                    Calculate Route
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default MapTest;
+
+
+/*
+
+<input
+                type="text"
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
+            />
+            <ul>
+                {results.map((result, index) => (
+                    <li key={index}>
+                        {result.address.freeformAddress}
+                    </li>
+                ))}
+            </ul>*/
+
+            /*useEffect(() => {
         if (query.length > 2) {
             ttServices.services.fuzzySearch({
                 key: "AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU",
@@ -40,27 +119,4 @@ const MapTest = () => {
         } else {
             setResults([]);
         }
-    }, [query]);
-
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder="Search..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
-            />
-            <ul>
-                {results.map((result, index) => (
-                    <li key={index}>
-                        {result.address.freeformAddress}
-                    </li>
-                ))}
-            </ul>
-            <div id="map" style={{ height: '500px', width: '100%' }}></div>
-        </div>
-    );
-};
-
-export default MapTest;
+    }, [query]);*/
