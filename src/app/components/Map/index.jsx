f485762dab0935276387d2186e8cdacdfe39e7f6
JeNordahl <jesper.nordahl@hotmail.com>
2024-05-22 11:21:15 +0200

    Added routes between two markers

--- src/app/components/Map/index.jsx
+++ src/app/components/Map/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import tt from '@tomtom-international/web-sdk-maps';
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 import ttServices from '@tomtom-international/web-sdk-services';
@@ -7,25 +7,31 @@ const MapTest = () => {
     const mapElement = useRef();
     const map = useRef();
 
-    function createRoute() {
+    const [markers, setMarkers] = useState([]);
+
+    const addMarker = (event) => {
+        if (markers.length < 2) {
+            const newMarker = new tt.Marker().setLngLat(event.lngLat).addTo(map.current);
+            setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+        }
+    }
+
+    const createRoute = () => {
+        if (markers.length < 2) return;
+
+        const locations = markers.map(marker => marker.getLngLat());
+
         const routeOptions = {
             key: 'AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU',
-            locations: [
-                [4.91191, 52.36619],
-                [4.87631, 52.36366],
-            ],
+            locations,
             vehicleCommercial: true,
             vehicleHeading: 0,
         };
 
         ttServices.services.calculateRoute(routeOptions).then((response) => {
-            routeOptions.locations.forEach((location) => 
-                new tt.Marker().setLngLat(location).addTo(map.current)
-            );
-
             const geojson = response.toGeoJson();
             map.current.addLayer({
-                id: "route" + Math.random().toString(36).substr(2, 9),
+                id: "route",
                 type: "line",
                 source: {
                     type: "geojson",
@@ -39,25 +45,41 @@ const MapTest = () => {
 
             const bounds = new tt.LngLatBounds();
             geojson.features[0].geometry.coordinates.forEach((point) => {
-                bounds.extend(tt.LngLat.convert(point)); // creates a bounding area
+                bounds.extend(tt.LngLat.convert(point));
             });
             map.current.fitBounds(bounds, {
                 duration: 300,
                 padding: 50,
                 maxZoom: 14,
-            }); // zooms the map to the searched route
+            });
         });
-    }
+    };
+
+    const clear = () => {
+        markers.forEach(marker => marker.remove());
+        setMarkers([]);
+        removeRoute('route');
+    };
+
+    const removeRoute = (id) => {
+        if (map.current.getLayer(id)) {
+            map.current.removeLayer(id);
+            map.current.removeSource(id);
+        }
+    };
 
     useEffect(() => {
         map.current = tt.map({
             key: "AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU",
             container: mapElement.current,
+            center: [4.91191, 59.36619],
             zoom: 2,
         });
 
         map.current.addControl(new tt.FullscreenControl());
         map.current.addControl(new tt.NavigationControl());
+
+        map.current.on('click', addMarker);
 
         return () => {
             map.current.remove();
@@ -77,6 +99,15 @@ const MapTest = () => {
                     }}
                 >
                     Calculate Route
+                </button>
+                <button
+                    className="btn"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        clear();
+                    }}
+                >
+                    Clear
                 </button>
             </div>
         </div>
@@ -103,20 +134,20 @@ export default MapTest;
                 ))}
             </ul>*/
 
-            /*useEffect(() => {
-        if (query.length > 2) {
-            ttServices.services.fuzzySearch({
-                key: "AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU",
-                query: query,
-                limit: 5,
-            }).go().then(response => {
-                setResults(response.results);
-                console.log(response.results);
-            }).catch(error => {
-                console.error('Error fetching search results:', error);
-                setResults([]);
-            });
-        } else {
-            setResults([]);
-        }
-    }, [query]);*/
+/*useEffect(() => {
+if (query.length > 2) {
+ttServices.services.fuzzySearch({
+    key: "AYZjZsp49t0NLJRpgZM77rW2VqGbKyfU",
+    query: query,
+    limit: 5,
+}).go().then(response => {
+    setResults(response.results);
+    console.log(response.results);
+}).catch(error => {
+    console.error('Error fetching search results:', error);
+    setResults([]);
+});
+} else {
+setResults([]);
+}
+}, [query]);*/
